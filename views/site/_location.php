79d81840dce309a44369502f054fd6363e828195
argenis1763 <argenis1763@github.com>
2015-09-02 01:00:50 -0430

    reponsive

--- views/site/_location.php
+++ views/site/_location.php
@@ -5,7 +5,7 @@
                 <div class="content-page">
                     <div class="row">
                         <div class="col-md-12">
-                            <div id="map" class="gmaps" style="height:410px; margin-top: 60px; margin-left: 12em; margin-right: 12em;"></div>
+                            <div id="map" class="gmaps" style="height: 100%;padding-top: 15%; padding-bottom: 21%;;margin-top: 5%; margin-left: 15%; margin-right: 15%;"></div>
                             <div id="info-map" class="row-fluid"><p>Situado en la Calle Alto Hatillo, Urbanización Los Naranjos. Caracas, Venezuela</p></div>
                         </div>                
                     </div>
--- views/site/_video.php
+++ views/site/_video.php
@@ -5,7 +5,7 @@
                 <div class="content-page">
                     <div class="row-fluid">
                         <div class="row-fluid col-md-12" style="text-align: center;">
-                            <iframe class="row-fluid" style="margin-bottom: 40px; margin-top: 20px; width: 100%;" height="540"
+                            <iframe class="row-fluid" style="margin-bottom: 2.5%; margin-top: 2.5%; width: 100%;" height="540"
                                     src="https://www.youtube.com/embed/AQhsmphARGw?autoplay=0">
                             </iframe>
                         </div>                
